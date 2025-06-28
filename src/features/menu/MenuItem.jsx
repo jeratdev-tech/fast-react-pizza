1cd955fc40a8d7252f70476cc8907060311a7ce6
stephen james <jeratdevworks@gmail.com>
2025-06-28 12:23:13 +0100

    applayouy and fetching data

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,4 @@
-import formatCurrency from "../../utils/formatCurrency";
+import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
