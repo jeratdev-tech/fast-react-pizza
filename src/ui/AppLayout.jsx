637df692c3369966f3f3d8647d5c948e5f3adbe9
stephen james <jeratdevworks@gmail.com>
2025-06-29 02:10:16 +0100

    writting data with react router actions

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
 import Header from "./Header";
+import Loader from "./Loader";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function AppLayout() {
+  const navigation = useNavigate();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
